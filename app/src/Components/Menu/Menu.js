d0a2bfcf5ac53e3868c0e6a428486ed30a627ef8
straengel <straengel@gmail.com>
2020-11-05 16:15:40 +0300

    Add lesson

---
+++ app/src/Components/Menu/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import dbMenu from '../DBMenu';
+import ListItem from './ListItem';
+import bannerImg from '../../img/banner.png';
+
+const MenuStyled = styled.main`
+  background-color: #ccc;
+  margin-top: 80px;
+  margin-left: 380px;
+`;
+
+const SectionMenu = styled.section`
+  padding: 30px;
+`;
+
+const MenuBanner = styled.div`
+  width: 100%;
+  height: 210px;
+  background-image: url(${bannerImg});
+  background-size: cover;
+  background-position: center;
+`;
+
+const Menu = ({ setOpenItem }) => (
+  <MenuStyled>
+    <MenuBanner/>
+    <SectionMenu>
+      <h2>Бургеры</h2>
+      <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
+    </SectionMenu>
+    <SectionMenu>
+      <h2>Закуски / Напитки</h2>
+      <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
+    </SectionMenu>
+  </MenuStyled>
+);
+
+export default Menu;
---
+++ app/src/Components/Style/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #299B01;
+  border: 2px solid #299B01;
+  font-size: 21px;
+  color: white;
+  padding: 18px 0;
+  width: 246px;
+  transition: all 0.3s linear;
+  margin: 0 auto;
+
+  &:hover {
+    background-color: transparent;
+    color: #299B01;
+  }
+`;
