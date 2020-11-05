3acd00de851378c6c2c3412a869460d903581765
straengel <straengel@gmail.com>
2020-11-05 16:12:47 +0300

    Final

--- app/src/Components/Button.js
+++ app/src/Components/Button.js
@@ -9,7 +9,6 @@ const AddButton = styled.button`
   padding: 18px 0;
   width: 246px;
   transition: all 0.3s linear;
-  margin: 0 auto;
 
   &:hover {
     background-color: transparent;
@@ -18,7 +17,7 @@ const AddButton = styled.button`
 `;
 
 const Button = ({ text }) => (
-  <AddButton>{text}</AddButton>
+	<AddButton>{text}</AddButton>
 );
 
 export default Button;
